2f0892634b01c7c2aca6e8f3b11ec54cbac212d5
SpanishWoW <spanishwow62@gmail.com>
2012-07-24 15:53:40 +0200

    [SPC01] Implementada la Armeria.

--- src/game/AchievementMgr.cpp
+++ src/game/AchievementMgr.cpp
@@ -2894,6 +2894,10 @@ void AchievementMgr::CompletedAchievement(AchievementEntry const* achievement)
     DETAIL_LOG("AchievementMgr::CompletedAchievement(%u)", achievement->ID);
     if((achievement->flags & ACHIEVEMENT_FLAG_COUNTER) || m_completedAchievements.find(achievement->ID)!=m_completedAchievements.end())
         return;
+		/** World of Warcraft Armory **/
+		if (sWorld.getConfig(CONFIG_BOOL_ARMORY_SUPPORT))
+			GetPlayer()->WriteWowArmoryDatabaseLog(1, achievement->ID);
+		/** World of Warcraft Armory **/
 
     SendAchievementEarned(achievement);
     CompletedAchievementData& ca =  m_completedAchievements[achievement->ID];
